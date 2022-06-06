b061622b536ccd02a7868b5b278ac929db25f8a3
andy_lin <andy_lp1995@163.com>
2022-06-06 09:46:07 +0800

    feat:first commit

---
+++ src/main/java/com/learner/leetcodelearner/LeetcodeLearnerApplication.java
@@ -0,0 +1,13 @@
+package com.learner.leetcodelearner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeetcodeLearnerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeetcodeLearnerApplication.class, args);
+    }
+
+}
