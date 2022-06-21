4dd2483e71bfd0e1dc7e672110ac15411f5d3e14
andy_lp <andy_lp1995@163.com>
2022-06-21 23:31:23 +0800

    feat:剑指_48

---
+++ src/main/java/com/learner/leetcodelearner/lib/swords/Swords_48.java
@@ -0,0 +1,66 @@
+package com.learner.leetcodelearner.lib.swords;
+//请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。
+//
+//
+//
+// 示例 1:
+//
+// 输入: "abcabcbb"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+//
+//
+// 示例 2:
+//
+// 输入: "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+//
+//
+// 示例 3:
+//
+// 输入: "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+//
+//
+//
+//
+// 提示：
+//
+//
+// s.length <= 40000
+//
+//
+// 注意：本题与主站 3 题相同：https://leetcode-cn.com/problems/longest-substring-without-
+//repeating-characters/
+// Related Topics 哈希表 字符串 滑动窗口 👍 450 👎 0
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Swords_48 {
+    /**
+     * 滑动窗口
+     * @param s
+     * @return
+     */
+    public int lengthOfLongestSubstring(String s) {
+        Set<Character> occ = new HashSet<Character>();
+        int n = s.length();
+        int rk = -1, ans = 0;
+        for (int i = 0; i < n; i++) {
+            if ( i != 0) {
+                occ.remove(s.charAt(i - 1));
+            }
+            while (rk + 1 < n && !occ.contains(s.charAt(rk + 1))) {
+                occ.add(s.charAt(rk + 1));
+                ++rk;
+            }
+            ans = Math.max(ans, rk - i + 1);
+        }
+        return ans;
+
+    }
+}
