2a44d39e5a3334a3a5fabc946f0c274939c9b83b
andy_lp <andy_lp1995@163.com>
2022-07-05 00:34:27 +0800

    feat:409

---
+++ src/main/java/com/learner/leetcodelearner/lib/easy/No_409.java
@@ -0,0 +1,60 @@
+package com.learner.leetcodelearner.lib.easy;
+
+public class No_409 {
+//给定一个包含大写字母和小写字母的字符串 s ，返回 通过这些字母构造成的 最长的回文串 。
+//
+// 在构造过程中，请注意 区分大小写 。比如 "Aa" 不能当做一个回文字符串。
+//
+//
+//
+// 示例 1:
+//
+//
+//输入:s = "abccccdd"
+//输出:7
+//解释:
+//我们可以构造的最长的回文串是"dccaccd", 它的长度是 7。
+//
+//
+// 示例 2:
+//
+//
+//输入:s = "a"
+//输入:1
+//
+//
+// 示例 3:
+//
+//
+//输入:s = "bb"
+//输入: 2
+//
+//
+//
+//
+// 提示:
+//
+//
+// 1 <= s.length <= 2000
+// s 只能由小写和/或大写英文字母组成
+//
+// Related Topics 贪心 哈希表 字符串 👍 425 👎 0
+    public int longestPalindrome(String s) {
+        int[] count = new int[128];
+        int length = s.length(), ans = 0;
+        for (int i = 0; i < length; ++i) {
+        // 遍历 统计每个字符出现次数
+            count[s.charAt(i)]++;
+        }
+
+        for (int v : count) {
+            // 遍历 积累偶数和
+            ans += v / 2 * 2;
+            if (v % 2 == 1 && ans % 2 == 0) {
+                // 回文串中仅可以存在一个奇数字符
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
