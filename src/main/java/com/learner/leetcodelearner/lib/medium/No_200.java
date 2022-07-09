32a8acaf3eaca9c3e68194b2b5504cbe9d53ebe4
andy_lp <andy_lp1995@163.com>
2022-07-09 14:25:06 +0800

    feat:70/200/509

---
+++ src/main/java/com/learner/leetcodelearner/lib/medium/No_200.java
@@ -0,0 +1,79 @@
+package com.learner.leetcodelearner.lib.medium;
+
+public class No_200 {
+//给你一个由 '1'（陆地）和 '0'（水）组成的的二维网格，请你计算网格中岛屿的数量。
+//
+// 岛屿总是被水包围，并且每座岛屿只能由水平方向和/或竖直方向上相邻的陆地连接形成。
+//
+// 此外，你可以假设该网格的四条边均被水包围。
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：grid = [
+//  ["1","1","1","1","0"],
+//  ["1","1","0","1","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","0","0","0"]
+//]
+//输出：1
+//
+//
+// 示例 2：
+//
+//
+//输入：grid = [
+//  ["1","1","0","0","0"],
+//  ["1","1","0","0","0"],
+//  ["0","0","1","0","0"],
+//  ["0","0","0","1","1"]
+//]
+//输出：3
+//
+//
+//
+//
+// 提示：
+//
+//
+// m == grid.length
+// n == grid[i].length
+// 1 <= m, n <= 300
+// grid[i][j] 的值为 '0' 或 '1'
+//
+// Related Topics 深度优先搜索 广度优先搜索 并查集 数组 矩阵 👍 1786 👎 0
+    public static void main(String[] args) {
+        char[][] grid = new char[][] {{'1','1','1','1','0'},{'1','1','0','1','0'},{'1','1','0','0','0'},{'0','0','0','0','0'}};
+        int i = numIslands(grid);
+    }
+    public static int numIslands(char[][] grid) {
+        if (grid == null || grid.length == 0) {
+            return 0;
+        }
+        int count = 0;
+        int m = grid.length, n = grid[0].length;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    dfs(grid, i , j);
+                }
+            }
+        }
+        return count;
+    }
+
+    public static void dfs(char[][] grid, int i, int j) {
+        if (i < 0 || j < 0 ||i >= grid.length || j >= grid[0].length || grid[i][j] == '0') {
+            return;
+        }
+        // 标记已遍历的点 正好改成0既可以保证不会重复搜索又可以保证岛屿的独立
+        grid[i][j] = '0';
+        dfs(grid, i + 1, j);
+        dfs(grid, i - 1, j);
+        dfs(grid, i, j + 1);
+        dfs(grid, i, j - 1);
+    }
+}
