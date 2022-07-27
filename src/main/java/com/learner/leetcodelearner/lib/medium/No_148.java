6a9188837a11ec856a4e855bf21797f6dd713827
andy_lp <andy_lp1995@163.com>
2022-07-28 00:32:07 +0800

    feat:148

---
+++ src/main/java/com/learner/leetcodelearner/lib/medium/No_148.java
@@ -0,0 +1,137 @@
+package com.learner.leetcodelearner.lib.medium;
+
+import com.learner.leetcodelearner.lib.bean.ListNode;
+
+public class No_148 {
+//给你链表的头结点 head ，请将其按 升序 排列并返回 排序后的链表 。
+//
+//
+//
+//
+//
+//
+// 示例 1：
+//
+//
+//输入：head = [4,2,1,3]
+//输出：[1,2,3,4]
+//
+//
+// 示例 2：
+//
+//
+//输入：head = [-1,5,3,4,0]
+//输出：[-1,0,3,4,5]
+//
+//
+// 示例 3：
+//
+//
+//输入：head = []
+//输出：[]
+//
+//
+//
+//
+// 提示：
+//
+//
+// 链表中节点的数目在范围 [0, 5 * 10⁴] 内
+// -10⁵ <= Node.val <= 10⁵
+//
+//
+//
+//
+// 进阶：你可以在 O(n log n) 时间复杂度和常数级空间复杂度下，对链表进行排序吗？
+// Related Topics 链表 双指针 分治 排序 归并排序 👍 1722 👎 0
+    public ListNode sortList(ListNode head) {
+        if (head == null || head.next == null)
+            return head;
+        ListNode fast = head.next, slow = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        ListNode tmp = slow.next;
+        slow.next = null;
+        ListNode left = sortList(head);
+        ListNode right = sortList(tmp);
+        ListNode h = new ListNode(0);
+        ListNode res = h;
+        while (left != null && right != null) {
+            if (left.val < right.val) {
+                h.next = left;
+                left = left.next;
+            } else {
+                h.next = right;
+                right = right.next;
+            }
+            h = h.next;
+        }
+        h.next = left != null ? left : right;
+        return res.next;
+    }
+
+
+    public ListNode sortList2(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        int length = 0;
+        ListNode node = head;
+        while (node != null) {
+            length++;
+            node = node.next;
+        }
+        ListNode dummyHead = new ListNode(0, head);
+        for (int subLength = 1; subLength < length; subLength <<= 1) {
+            ListNode prev = dummyHead, curr = dummyHead.next;
+            while (curr != null) {
+                ListNode head1 = curr;
+                for (int i = 1; i < subLength && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode head2 = curr.next;
+                curr.next = null;
+                curr = head2;
+                for (int i = 1; i < subLength && curr != null && curr.next != null; i++) {
+                    curr = curr.next;
+                }
+                ListNode next = null;
+                if (curr != null) {
+                    next = curr.next;
+                    curr.next = null;
+                }
+                ListNode merged = merge(head1, head2);
+                prev.next = merged;
+                while (prev.next != null) {
+                    prev = prev.next;
+                }
+                curr = next;
+            }
+        }
+        return dummyHead.next;
+    }
+
+    public ListNode merge(ListNode head1, ListNode head2) {
+        ListNode dummyHead = new ListNode(0);
+        ListNode temp = dummyHead, temp1 = head1, temp2 = head2;
+        while (temp1 != null && temp2 != null) {
+            if (temp1.val <= temp2.val) {
+                temp.next = temp1;
+                temp1 = temp1.next;
+            } else {
+                temp.next = temp2;
+                temp2 = temp2.next;
+            }
+            temp = temp.next;
+        }
+        if (temp1 != null) {
+            temp.next = temp1;
+        } else if (temp2 != null) {
+            temp.next = temp2;
+        }
+        return dummyHead.next;
+    }
+}
+
