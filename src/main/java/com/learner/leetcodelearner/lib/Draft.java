9003f443f9ada2db39c58610a80cd5926c466f1d
andy_lin <andy_lp1995@163.com>
2023-03-09 19:20:48 +0800

    线程测试

--- src/main/java/com/learner/leetcodelearner/lib/Draft.java
+++ src/main/java/com/learner/leetcodelearner/lib/Draft.java
@@ -1,6 +1,11 @@
 package com.learner.leetcodelearner.lib;
 
+import com.learner.leetcodelearner.lib.bean.Node;
+
 import java.io.*;
+import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.stream.Collectors;
 
 /**
  * @Description
@@ -8,9 +13,51 @@ import java.io.*;
  * @Date: 2022/09/01 15:31
  **/
 public class Draft {
-    public static void main(String[] args) throws IOException {
+    private static ThreadLocal<String> threadLocal = new ThreadLocal<>();
+    private static LinkedList<String> threadList = new LinkedList<>();
+    public static void main(String[] args) throws IOException, InterruptedException {
+//        List<Integer> list = new ArrayList<>();
+//        list.add(1);
+//        list.add(4);
+//        list.add(5);
+//        list.add(5);
+//        list.add(3);
+//        list.add(3);
+//        list.add(2);
+//
+//        // 去重并降序排序
+//        List<Integer> result = list.stream()
+//                .distinct()
+//                .sorted(Collections.reverseOrder()).collect(Collectors.toList());
+//
+//
+//
+//        System.out.println(result); // 输出 [5, 3, 2, 1]
+//        result.add(6);
+//        System.out.println(result); // 输出 [5, 3, 2, 1]
+        Thread thread = new Thread(() -> {
+            while (!Thread.currentThread().isInterrupted()) {
+                System.out.println("Thread is running...");
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt(); // 中断当前线程
+//                    Thread.interrupted();
+                    System.out.println("Thread is interrupted!");
+                }
+            }
+        });
 
+        thread.start();
+        Thread.sleep(5000);
+        thread.interrupt();
     }
+
+
+
+
+
+
 }
 
 
